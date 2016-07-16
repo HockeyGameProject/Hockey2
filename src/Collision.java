923a6cc2652eb4e6b73ddffb2070f3dafe96345e
gatorsgonnagait <evmesa@gmail.com>
2016-07-15 17:16:01 -0700

    collision, puck reflection

---
+++ src/Collision.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Mesa on 7/15/2016.
+ */
+public class Collision {
+
+    public boolean objectsCollide(MovingObject object1, MovingObject object2){
+        int distance = object1.radius + object2.radius;
+        if(Math.sqrt(Math.pow((object2.location.x - object1.location.x), 2) + Math.pow((object2.location.y - object1.location.y), 2)) == distance){
+            return true;
+        }
+        return false;
+    }
+
+    //public boolean edgeOfRinkCollision(MovingObject object1){// need a different one for players and puck
+
+    //}
+}
--- src/Puck.java
+++ src/Puck.java
@@ -7,6 +7,12 @@ import java.awt.*;
  */
 public class Puck extends MovingObject {
 
+    int topBoundary = 100;
+    int bottomBoundary = 450;
+    int leftBoundary = 100;
+    int rightBoundary = 900;
+    double reflectAngle;
+
     public Puck(Point point, int speed, double angle, int radius, Color color) {
         super(point, speed, angle, radius, color);
     }
@@ -16,8 +22,16 @@ public class Puck extends MovingObject {
         super.setRadius(15);
     }
 
+    public void reflection(){
+        if(location.x == topBoundary || location.x == bottomBoundary || location.y == leftBoundary || location.y == rightBoundary ){
+            reflectAngle = (-1)*getAngle();
+            setAngle(reflectAngle);
+        }
+    }
+
     @Override
     public void updateLocation() {
+        reflection();
         location.x = (int) (location.x + getSpeed() * Math.sin(getAngle()));
         location.y = (int) (location.y + getSpeed() * Math.cos(getAngle()));
     }
