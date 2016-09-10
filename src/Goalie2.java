2ff6891af34bf98b0f158d422f31f19b6d0231a1
gatorsgonnagait <evmesa@gmail.com>
2016-09-09 17:13:47 -0700

    made variable for 4, increased grab area

--- src/Goalie2.java
+++ src/Goalie2.java
@@ -33,39 +33,34 @@ public class Goalie2 extends Player {
         }
     }
 
+    @Override
     public void updateLocation() {
         stick.updateLocation();
         location.x = rightGoalLine - 15;
+
         double Y = puck.location.y - location.y;//makes it face puck
         double X = puck.location.x - location.x;
 
 
-        double puckAngle = Math.atan2(Y,X);
-
 
         // if puck is behind goal line
         if (puck.location.x >= rightGoalLine){
-            if ( puck.location.y < horizontalMiddle){
-                moveGoalieUp();
-            }
-            else if ( puck.location.y > horizontalMiddle){
-                moveGoalieDown();
-            }
-
 
             if(puck.location.y < horizontalMiddle){
-                setAngle(angle + 4*Math.PI/180);
-                if(angle >= 3*Math.PI/2){
+                moveGoalieUp();
+                setAngle(angle + 4*Math.PI/180);//rotates clockwise
+                System.out.println(angle);
+                if(angle >= -Math.PI/2){
                     setAngle( 3*Math.PI/2);
                 }
             }
             else if(puck.location.y > horizontalMiddle){
-                setAngle(angle - 4*Math.PI/180);
+                moveGoalieDown();
+                setAngle(angle - 4*Math.PI/180);//roates goalie coutner clockwise
                 if(angle <= Math.PI/2){
-                    setAngle( Math.PI/2);
+                    setAngle( -3*Math.PI/2);// i dont know how this works
                 }
             }
-
         }
         else {
             setAngle(Math.atan2(Y, X));
@@ -78,5 +73,7 @@ public class Goalie2 extends Player {
 
         }
     }
+
+
 
 }
